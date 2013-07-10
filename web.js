41f5d5ff59daf67464927eb969dcfca3a2f4e136
casperyap <casyap@gmail.com>
2013-07-10 13:59:51 +0000

    Change web.js to read from Index

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var bufIndexFile = fs.readFileSync('Index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(bufIndexFile.toString());
 });
 
 var port = process.env.PORT || 5000;
